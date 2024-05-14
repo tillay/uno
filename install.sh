0939ee6653f5d856c3d6e8f29b4158fe2111b426
Tilley <156486795+tillay@users.noreply.github.com>
2024-05-14 09:58:55 -0600

    added wild fix for macos

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+
 replace_in_files() {
     sed -i '' -e 's/-e /$/g' -e 's/\\e\[0m//g' "$@"
-    echo Modified $@ for macos
+    echo "Modified $@ for macOS"
 }
+
 if [[ "$(uname)" == "Darwin" ]]; then
     echo "Detected macOS"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew install figlet
     brew install neofetch
-    replace_in_files 0 1 2 3 4 5 6 7 8 9 10 e
+    replace_in_files 0 1 2 3 4 5 6 7 8 9 e
+    # Overwrite card 10 with new content
+    echo $'\e['35'm ++++++++++' > 10
+    echo $'\e['35'm#####+++++++' >> 10
+    echo $'\e['35'm##        ++' >> 10
+    echo $'\e['35'm##  wild  ++' >> 10
+    echo $'\e['35'm##        ++' >> 10
+    echo $'\e['35'm##        ++' >> 10
+    echo $'\e['35'm##  wild  ++' >> 10
+    echo $'\e['35'm##        ++' >> 10
+    echo $'\e['35'm#######+++++' >> 10
+    echo $'\e['35'm ##########' >> 10
 elif [ -x "$(command -v apt)" ]; then
     echo "Detected Debian-based distribution"
     sudo apt install figlet
@@ -24,4 +37,5 @@ elif [ -x "$(command -v pacman)" ]; then
 else
     echo "Unsupported distribution or package manager not found."
 fi
-chmod u+x 0 1 2 3 4 5 6 7 8 9 10 e cards.sh
+
+chmod u+x 0 1 2 3 4 5 6 7 8 9 e cards.sh
