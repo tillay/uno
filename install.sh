48ce3a9ad48190ed9a0a6253e461e758a1b93ba8
tillay <tilleyy8@gmail.com>
2024-05-14 09:46:25 -0600

    installation overhaul

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
-sudo apt install figlet
-sudo apt install neofetch
-sudo dnf install figlet
-sudo dnf install neofetch
-sudo pacman -S figlet
-sudo pacman -S neofetch
-#echo -e '\e['34'mDependencies installed\e[0m'
-chmod u+x 1
-chmod u+x 0
-chmod u+x 2
-chmod u+x 3
-chmod u+x 4
-chmod u+x 5
-chmod u+x e
-chmod u+x 6
-chmod u+x 7
-chmod u+x 8
-chmod u+x 9
-chmod u+x 10
-#echo -e '\e['$1'cards initalized\e[0m'
-chmod u+x cards.sh
-#echo 'alias uno='cd ~/Documents/code/uno&&python3 main.py'' >> ~/.bashrc
+#!/bin/bash
+replace_in_files() {
+    sed -i '' -e 's/-e /$/g' -e 's/\\e\[0m//g' "$@"
+    echo Modified $@ for macos
+}
+if [[ "$(uname)" == "Darwin" ]]; then
+    echo "Detected macOS"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install figlet
+    brew install neofetch
+    replace_in_files 0 1 2 3 4 5 6 7 8 9 10 e
+elif [ -x "$(command -v apt)" ]; then
+    echo "Detected Debian-based distribution"
+    sudo apt install figlet
+    sudo apt install neofetch
+elif [ -x "$(command -v dnf)" ]; then
+    echo "Detected Fedora-based distribution"
+    sudo dnf install figlet
+    sudo dnf install neofetch
+elif [ -x "$(command -v pacman)" ]; then
+    echo "Detected Arch-based distribution"
+    sudo pacman -S figlet
+    sudo pacman -S neofetch
+else
+    echo "Unsupported distribution or package manager not found."
+fi
+chmod u+x 0 1 2 3 4 5 6 7 8 9 10 e cards.sh
