d84460d26135ba4df843700eb5d3fedef2f28c84
tillay <tilley@big-tilley.us-central1-c.c.ray-romano-420100.internal>
2024-09-04 03:15:34 +0000

    fixed minor issues

--- install.sh
+++ install.sh
@@ -1,55 +1,50 @@
 #!/bin/bash
 
-replace_in_files() {
+replace_in_file() {
     sed -i '' -e 's/-e /$/g' -e 's/\\e\[0m//g' "$@"
 }
 
 if [[ "$(uname)" == "Darwin" ]]; then
     echo "Detected macOS"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    brew install figlet
-    brew install neofetch
-    replace_in_files 0 1 2 3 4 5 6 7 8 9 10 e
-        cat > 10 <<EOL
-echo \$'\e['35'm ++++++++++'
-echo \$'\e['35'm#####+++++++'
-echo \$'\e['35'm##        ++'
-echo \$'\e['35'm##  wild  ++'
-echo \$'\e['35'm##        ++'
-echo \$'\e['35'm##        ++'
-echo \$'\e['35'm##  wild  ++'
-echo \$'\e['35'm##        ++'
-echo \$'\e['35'm#######+++++'
-echo \$'\e['35'm ##########'
+    echo "Replacing wildcard art with macOS version"
+    
+    temp_card_file=$(mktemp)
+
+    cat > "$temp_card_file" <<EOL
+echo \$'\e['35'm ++++++++++ \e[0m\${spaces}'
+echo \$'\e['35'm#####+++++++ \e[0m\${spaces}'
+echo \$'\e['35'm##        ++ \e[0m\${spaces}'
+echo \$'\e['35'm##  wild  ++ \e[0m\${spaces}'
+echo \$'\e['35'm##        ++ \e[0m\${spaces}'
+echo \$'\e['35'm##        ++ \e[0m\${spaces}'
+echo \$'\e['35'm##  wild  ++ \e[0m\${spaces}'
+echo \$'\e['35'm##        ++ \e[0m\${spaces}'
+echo \$'\e['35'm#######+++++ \e[0m\${spaces}'
+echo \$'\e['35'm ########## \e[0m\${spaces}'
 EOL
-elif [ -x "$(command -v apt)" ]; then
-    echo "Detected Debian-based distribution"
-    sudo apt install figlet
-    sudo apt install neofetch
-elif [ -x "$(command -v dnf)" ]; then
-    echo "Detected Fedora-based distribution"
-    sudo dnf install figlet
-    sudo dnf install neofetch
-elif [ -x "$(command -v pacman)" ]; then
-    echo "Detected Arch-based distribution"
-    sudo pacman -S figlet
-    sudo pacman -S neofetch
-else
-    echo "Unsupported distribution or package manager not found."
+
+    sed -i '' -e "/echo -e \"\\e\[90m ++++++++++ \\e\[0m\${spaces}\"/ {
+        r $temp_card_file
+        d
+    }" 10
+
+    rm "$temp_card_file"
+
+    read -p "Play Singleplayer (1) or Against Computer (2): " mode
+    read -p "Number of cards to start with: " deal
+    if [[ "$mode" == "1" ]]; then
+        replacement="game()"
+    elif [[ "$mode" == "2" ]]; then
+        replacement="bot()"
+    else
+        echo "Invalid option. Exiting."
+        exit 1
+    fi
+    sed -i '' -e "s|print(\"Please run install.sh first!\")|$replacement|g" main.py
+    sed -i '' -e "s/deal = 6/deal = $deal/g" main.py
+    echo "In the future, run python3 main.py to play!"
+    for file in ~/*h_history; do echo 'python3 main.py' >> "$file"; done
+    sleep 1
+    python3 main.py
 fi
-read -p "Play Singleplayer (1) or Against Computer (2): " mode
-read -p "Number of cards to start with: " deal
-if [[ "$mode" == "1" ]]; then
-    replacement="game()"
-elif [[ "$mode" == "2" ]]; then
-    replacement="bot()"
-else
-    echo "Invalid option. Exiting."
-    exit 1
-fi
-sed -i '' -e "s|print(\"Please run install.sh first!\")|$replacement|g" main.py
-sed -i '' -e "s/deal = 6/deal = $deal/g" main.py
-chmod u+x 0 1 2 3 4 5 6 7 8 9 10 e cards.sh
-echo "in the future, run python3 main.py to play!"
-sleep 3
-python3 main.py
+
